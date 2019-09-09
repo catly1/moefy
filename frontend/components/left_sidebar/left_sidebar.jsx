e5a7ece1fae38dd506f30c67936e51414a315c82
Carlos Catly <ccatly@gmail.com>
2019-09-09 15:22:32 -0700

    dones some prelimiary sidebar with no working links/functionality

--- frontend/components/left_sidebar/left_sidebar.jsx
+++ frontend/components/left_sidebar/left_sidebar.jsx
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
+import { MdHome, MdSearch } from "react-icons/md";
+import { GoPerson } from "react-icons/go";
+import { GiBookshelf } from "react-icons/gi";
+import { FaPlus } from "react-icons/fa";
+import { FiArrowDownCircle } from "react-icons/fi";
+
 
 class LeftSidebar extends Component {
 
@@ -12,10 +18,32 @@ class LeftSidebar extends Component {
                     <img src="./assets/logo.png" alt="Moefy" />
                 </Link>
             </div>
-            <div className="user-profile">
-                <Link to='/player/settings/account'>
-                    {this.props.currentUser.username}
+
+            <nav className="sidebar-main-nav">
+                <Link className="left-nav-bar-items" to='/player/browse' >
+                    <MdHome/><div>Home</div>
                 </Link>
+                <Link className="left-nav-bar-items" to='/player/browse' >
+                    <MdSearch/><div>Search</div>
+                </Link>
+                <Link className="left-nav-bar-items" to='/player/browse' >
+                    <GiBookshelf/><div>Library</div>
+                </Link>
+            </nav>
+            <h2 className="sidebar-playlist-header">Playlists</h2>
+            <button className="create-playlist-button">
+                <FaPlus/>
+                <span>Create Playlist</span>
+            </button>
+            <div className="sidebar-bottom">
+                <Link className="left-nav-bar-items sidebar-bottom-download" to='/player/browse' >
+                    <FiArrowDownCircle/><div>Install App</div>
+                </Link>
+                <div className="user-profile">
+                    <Link className="left-nav-bar-items sidebar-bottom-download" to='/player/settings/account'>
+                        <GoPerson/><div>{this.props.currentUser.username}</div>
+                    </Link>
+                </div>
             </div>
         </div>
             </section>
