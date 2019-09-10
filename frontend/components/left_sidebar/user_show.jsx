d3a37dc7487c6b5948f0e42dba7bc37c3bbb51dc
Carlos Catly <ccatly@gmail.com>
2019-09-09 20:39:48 -0700

    finished the web player's profile page. Also adjust the gradient in some backgrounds

--- frontend/components/left_sidebar/user_show.jsx
+++ frontend/components/left_sidebar/user_show.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import { GoPerson } from "react-icons/go";
+import { Link } from 'react-router-dom';
 
 class UserShow extends Component {
     render(){
         return(
-            <div>
-                <button onClick={this.props.logout}>Logout</button>
+            <div className="player-main-view">
+                <div className="player-background player-background-user-profile"></div>
+                <div className="user-profile-page">
+                    <div className="profile-name-pic">
+                        <GoPerson />
+                        <div>{this.props.currentUser.username}</div>
+                    </div>
+                    <ul>
+                        <button className="splash-grn-button profile-button"><Link to="/player/settings/account">View account</Link></button>
+                        <button className="splash-grn-button profile-button"><Link to="/">Full website</Link></button>
+                        <button className="splash-grn-button profile-button"><Link to="/player/settings/account">Help</Link></button>
+                        <button onClick={this.props.logout} className="splash-grn-button profile-button">Logout</button>
+                    </ul>
+                </div>
             </div>
         )
     }
